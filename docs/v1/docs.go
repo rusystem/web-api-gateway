ad94bb37cf3434ee330cbf046768bf5ea5a4bddc
mdmitry <ru.system.ru@gmail.com>
2024-08-29 15:04:41 +0500

    fix swagger

--- docs/v1/docs.go
+++ docs/v1/docs.go
@@ -1034,7 +1034,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "91.243.71.100:8080",
 	BasePath:         "/api/web-api-gateway/v1",
 	Schemes:          []string{},
 	Title:            "Web api gateway API",
