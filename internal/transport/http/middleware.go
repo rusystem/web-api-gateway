e9a0bb2e27850bc1ac6a4c9cd4feb9d49020359d
mdmitry <ru.system.ru@gmail.com>
2024-09-11 01:03:28 +0500

    fix cors

--- internal/transport/http/middleware.go
+++ internal/transport/http/middleware.go
@@ -34,13 +34,13 @@ import (
 
 func corsMiddleware(c *gin.Context) {
 	c.Header("Access-Control-Allow-Origin", "*")
-	c.Header("Access-Control-Allow-Methods", "*")
-	c.Header("Access-Control-Allow-Headers", "*")
-	c.Header("Content-Type", "application/json")
+	c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
-	if c.Request.Method != "OPTIONS" {
-		c.Next()
-	} else {
+	if c.Request.Method == "OPTIONS" {
 		c.AbortWithStatus(http.StatusOK)
+		return
 	}
+
+	c.Next()
 }
